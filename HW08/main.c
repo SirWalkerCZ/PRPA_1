022657432a8fae2284804bde9ce7521464e9ace9
Merasmus <vip.nebu@gmail.com>
2024-01-13 21:48:59 +0100

    github backup bro

--- HW08/main.c
+++ HW08/main.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compareNumbers(const void *a, const void *b) {
+int compareNumbers(const void *a, const void *b)
+{
     return (*(int *)a - *(int *)b);
 }
 
-int* numbers = NULL;
+int *numbers = NULL;
 int arraySize = 0;
 int capacity = 0;
 int num;
 int histogram;
 
-
-int main(){
-    if (scanf("%i", &histogram)<1)
+int main()
+{
+    if (scanf("%i", &histogram) < 1)
     {
         fprintf(stderr, "Error: Chyba histogramu!\n");
         return 100;
     }
-    
-    while (scanf("%i", &num) == 1) {
-        if (arraySize == capacity) {
-            capacity += 10;
-            int* temp = realloc(numbers, capacity * sizeof(int));
 
-            if (temp == NULL) {
+    while (scanf("%i", &num) == 1)
+    {
+        if (arraySize == capacity)
+        {
+            capacity += 10;
+            int *temp = realloc(numbers, capacity * sizeof(int));
+
+            if (temp == NULL)
+            {
                 fprintf(stderr, "Error: Memory allocation failed\n");
                 free(numbers);
                 return 1;
@@ -44,58 +48,58 @@ int main(){
     // printf("\n");
     qsort(numbers, arraySize, sizeof(numbers[0]), compareNumbers);
     float median;
-    (arraySize % 2 == 0) ? (median = (float)numbers[arraySize/2]) : (median = ((float)(numbers[arraySize/2] + numbers[(arraySize/2)+1])) / 2.0);
-    printf("Median %.2f\nPocet cisel: %i\nMin. hodnota: %i\nMax. hodnota: %i\n", median, arraySize, numbers[0], numbers[arraySize-1]);
-    float size = (float)(numbers[arraySize-1]-numbers[0])/histogram;
+    (arraySize % 2 == 0) ? (median = (float)numbers[arraySize / 2]) : (median = ((float)(numbers[arraySize / 2] + numbers[(arraySize / 2) + 1])) / 2.0);
+    printf("Median %.2f\nPocet cisel: %i\nMin. hodnota: %i\nMax. hodnota: %i\n", median, arraySize, numbers[0], numbers[arraySize - 1]);
+    float size = (float)(numbers[arraySize - 1] - numbers[0]) / histogram;
     printf("Histogram:\n");
-    float edgeCases[histogram+1];
+    float edgeCases[histogram + 1];
 
-    //vytvoření limitů histogramu
+    // vytvoření limitů histogramu
     for (size_t i = 0; i < histogram; i++)
     {
-        edgeCases[i] = (float)numbers[0]+(float)i*size;
+        edgeCases[i] = (float)numbers[0] + (float)i * size;
     }
-    edgeCases[histogram] = numbers[arraySize-1];
+    edgeCases[histogram] = numbers[arraySize - 1];
 
-    //vytvoření hodnot histogramu
-    int *histogramValues= calloc(histogram, sizeof(int));
+    // vytvoření hodnot histogramu
+    int *histogramValues = calloc(histogram, sizeof(int));
     for (size_t i = 0; i < arraySize; i++)
     {
-        int bin_result = (numbers[i]-numbers[0])/size;
-        if (bin_result ==histogram)
+        int bin_result = (numbers[i] - numbers[0]) / size;
+        if (bin_result == histogram)
         {
-           bin_result--;
+            bin_result--;
         }
-        //printf("binresult: %i\n", bin_result);
+        // printf("binresult: %i\n", bin_result);
         histogramValues[bin_result]++;
     }
 
-    //tvorba koeficientu na lineární škálování
+    // tvorba koeficientu na lineární škálování
     int maxHisVal = -__INT_MAX__;
-    int loc = 0; //lokace maximální hodnoty histogramu
+    int loc = 0; // lokace maximální hodnoty histogramu
     for (size_t i = 0; i < histogram; i++)
     {
-        if (maxHisVal<histogramValues[i])
+        if (maxHisVal < histogramValues[i])
         {
             maxHisVal = histogramValues[i];
             loc = i;
-        }        
+        }
     }
-    double coeficient = (double)25/histogramValues[loc];
-    //printf("%f, max %i, test: %f\n", coeficient, maxHisVal, (double)maxHisVal*coeficient);
+    double coeficient = (double)25 / histogramValues[loc];
+    // printf("%f, max %i, test: %f\n", coeficient, maxHisVal, (double)maxHisVal*coeficient);
 
-    //vypsání histogramu
-    char *line =malloc(25*sizeof(char));
+    // vypsání histogramu
+    char *line = malloc(25 * sizeof(char));
     for (size_t i = 0; i < histogram; i++)
-    {    
-        int temp = (double)histogramValues[i]*coeficient;
-        //printf("%i\t", temp);
+    {
+        int temp = (double)histogramValues[i] * coeficient;
+        // printf("%i\t", temp);
         for (int j = 0; j < temp; j++)
         {
-            line[j]='=';
-        }      
-        line[temp]='\0';   
-        printf("%5.1f - %5.1f |%s\n", edgeCases[i], edgeCases[i+1], line);
+            line[j] = '=';
+        }
+        line[temp] = '\0';
+        printf("%5.1f - %5.1f |%s\n", edgeCases[i], edgeCases[i + 1], line);
     }
 
     free(numbers);
@@ -103,5 +107,4 @@ int main(){
     free(histogramValues);
 
     return 0;
-
 }
---
+++ HW09/main.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_WORD_LENGTH 20
+
+typedef struct
+{
+    char word[MAX_WORD_LENGTH + 1];
+    int count;
+} WordCount;
+
+void removePunctuation(char *word)
+{
+    int length = strlen(word);
+    for (int i = 0; i < length; i++)
+    {
+        if (ispunct(word[i]))
+        {
+            for (int j = i; j < length; j++)
+            {
+                word[j] = word[j + 1];
+            }
+            i--;
+            length--;
+        }
+    }
+}
+
+int compareWords(const void *a, const void *b)
+{
+    return ((WordCount *)b)->count - ((WordCount *)a)->count;
+}
+
+int compareWordsAlphabetically(const void *a, const void *b)
+{
+    return strcmp(((WordCount *)a)->word, ((WordCount *)b)->word);
+}
+
+    int caseSensitive = 0;
+    int sortByFrequency = 1;
+    int wordLengthLimit = -1;
+
+
+int main(int argc, char *argv[])
+{
+
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-c") == 0)
+        {
+            caseSensitive = 1;
+        }
+        else if (strcmp(argv[i], "-s") == 0)
+        {
+            i++;
+            if (i < argc)
+            {
+                if (strcmp(argv[i], "1") == 0)
+                {
+                    sortByFrequency = 1;
+                }
+                else if (strcmp(argv[i], "2") == 0)
+                {
+                    sortByFrequency = 0;
+                }
+                else
+                {
+                    fprintf(stderr, "Warning: Chybna hodnota parametru -s!\n");
+                    return 0;
+                }
+            }
+            else
+            {
+                fprintf(stderr, "Warning: Chybna hodnota parametru -s!\n");
+                return 0;
+            }
+        }
+        else if (strcmp(argv[i], "-l") == 0)
+        {
+            i++;
+            if (i < argc)
+            {
+                wordLengthLimit = atoi(argv[i]);
+                if (wordLengthLimit < 0)
+                {
+                    fprintf(stderr, "Warning: Chybna hodnota parametru -l!\n");
+                    return 0;
+                }
+            }
+            else
+            {
+                fprintf(stderr, "Warning: Chybna hodnota parametru -l!\n");
+                return 0;
+            }
+        }
+    }
+
+
+
+    WordCount *wordCounts = malloc(sizeof(WordCount));
+    int wordCount = 0;
+    char buffer[MAX_WORD_LENGTH + 1];
+
+    while (scanf("%s", buffer) == 1)
+    {
+        if (caseSensitive == 0) 
+        {
+            for (int i = 0; buffer[i]; i++)
+            {
+                buffer[i] = tolower(buffer[i]);
+            }
+        }
+
+        removePunctuation(buffer);
+
+        if (wordLengthLimit < 0 || (int)strlen(buffer) <= wordLengthLimit)
+        {
+            int found = 0;
+
+            for (int i = 0; i < wordCount; i++)
+            {
+                if (strcmp(wordCounts[i].word, buffer) == 0)
+                {
+                    wordCounts[i].count++;
+                    found = 1;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                wordCounts = realloc(wordCounts, (wordCount + 1) * sizeof(WordCount));
+                strcpy(wordCounts[wordCount].word, buffer);
+                wordCounts[wordCount].count = 1;
+                wordCount++;
+            }
+        }
+    }
+    
+
+    qsort(wordCounts, wordCount, sizeof(WordCount), compareWords);
+    char **leastUsedWord = malloc(wordCount*MAX_WORD_LENGTH*sizeof(char));
+
+    int shortestWords = 0;
+    int i = 1; 
+    while (shortestWords<1)
+    {        
+        for (size_t j = 0; j < wordCount; j++)
+        {
+            if (wordCounts[j].count==i)
+            {
+                leastUsedWord[shortestWords] = wordCounts->word;
+                shortestWords++;
+            }
+            
+        }
+        i++;
+            
+    }
+    
+
+    if (sortByFrequency != 1)
+    {
+        qsort(wordCounts, wordCount, sizeof(WordCount), compareWordsAlphabetically);
+    }
+    
+
+
+    printf("Seznam slov:\n");
+    for (int i = 0; i < wordCount; i++)
+    {
+        printf("%-20s %i\n", wordCounts[i].word, wordCounts[i].count);
+    }
+    printf("%-20s %i\n", "Pocet slov:", wordCount);
+    printf("%-20s ", "Nejmene caste:");
+    for (size_t i = 0; i < shortestWords; i++)
+    {
+        printf("%s ",leastUsedWord[i]);
+    }
+    
+
+
+    free(wordCounts);
+
+    return 0;
+}
